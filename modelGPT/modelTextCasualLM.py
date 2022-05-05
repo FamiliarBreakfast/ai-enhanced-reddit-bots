3ef27078be97d1bc5966555091b382c347834121
FamiliarBreakfast <48958412+FamiliarBreakfast@users.noreply.github.com>
2022-05-05 02:24:46 -0400

    Fix modelTextCasualLM

--- modelGPT/modelTextCasualLM.py
+++ modelGPT/modelTextCasualLM.py
@@ -10,18 +10,27 @@ import sys
 import logging
 
 class ModelTextCasualLM:
-	def __init__(self, model_path, tokenizer_path):
+	def __init__(self, model_path='gpt2', tokenizer_path='gpt2', device='cpu'):
+		self.model_path = model_path
+		self.tokenizer_path = tokenizer_path
+		self.device = device
+
 		logging.info('Loading model...')
-		model = AutoModelForCausalLM.from_pretrained('gpt2', # replace with your model (path or huggingface model)
+		self.model = AutoModelForCausalLM.from_pretrained(model_path,
 			##torch_dtype=torch.float16, # use float16 if on GPU
 			low_cpu_mem_usage=True)
 		logging.info('Loading tokenizer...')
-		tokenizer = AutoTokenizer.from_pretrained('gpt2') # replace with your tokenizer (path or huggingface model)
+		self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
 
 	#todo: add logging
 	#todo: pull from database
 
 	def parse(self, text):
+		#todo: add cleaning and prep to strings
+		tokens = self.tokenizer.encode(text)
+		return tokens
+	
+	def tokenize(self, text):
 		tokens = self.tokenizer.encode(text)
 		return tokens
 
