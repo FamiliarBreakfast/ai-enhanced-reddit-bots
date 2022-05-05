d38637adb7a65f1017e1d70da9f7a2f746a475d8
Anikyte <familiarbreakfast@gmail.com>
2022-05-05 00:57:55 -0400

    Create skeleton files

---
+++ praw.py
@@ -0,0 +1,9 @@
+import praw
+
+reddit = praw.Reddit(
+    client_id='', 
+    client_secret='', 
+    user_agent='', 
+    username='', 
+    password=''
+)
---
+++ run.py
@@ -0,0 +1,1 @@
+from modelGPT import modelTextCasualLM
