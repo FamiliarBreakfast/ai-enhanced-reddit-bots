d55226d42b2c85354634b39d3b98b6abbdc21743
Anikyte <familiarbreakfast@gmail.com>
2022-05-05 00:57:32 -0400

    Create modelTextGPT module - Handles GPT2 and GPTJ text generation

---
+++ modelGPT/modelTextCasualLM.py
@@ -0,0 +1,47 @@
+from lib2to3.pgen2.tokenize import generate_tokens
+from transformers import AutoTokenizer, AutoModelForCasualLM
+import torch
+import numpy as np
+
+import json
+import random
+
+# logging
+import sys
+import logging
+
+class ModelTextCasualLM:
+	def __init__(self, model_path, tokenizer_path):
+		logging.info('Loading model...')
+		model = AutoModelForCasualLM.from_pretrained('gpt2', # replace with your model (path or huggingface model)
+			##torch_dtype=torch.float16, # use float16 if on GPU
+			low_cpu_mem_usage=True)
+		logging.info('Loading tokenizer...')
+		tokenizer = AutoTokenizer.from_pretrained('gpt2') # replace with your tokenizer (path or huggingface model)
+
+	#todo: add logging
+	#todo: pull from database
+
+	def parse(self, text):
+		tokens = self.tokenizer.encode(text)
+		return tokens
+
+	def generate(self, tokens):
+		generate_tokens = self.model.generate(
+			input_ids=tokens,
+			max_length=200,
+			temperature=1.0,
+			do_sample=True,
+		)
+
+		return self.tokenizer.decode(generate_tokens, skip_special_tokens=False)
+	
+	def generate_tokens(self, tokens):
+		generate_tokens = self.model.generate(
+			input_ids=tokens,
+			max_length=200,
+			temperature=1.0,
+			do_sample=True,
+		)
+
+		return generate_tokens
