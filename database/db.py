ce4c6d9363859700c1adb57363d7f0f11b05e697
FamiliarBreakfast <48958412+FamiliarBreakfast@users.noreply.github.com>
2022-05-07 02:44:57 -0400

    Remove unnesescary modules and update example bot

--- database/db.py
+++ database/db.py
@@ -2,28 +2,28 @@ import sqlite3
 import json
 
 class BasicDB:
-    pass
+	def __init__(self, db_path, table_name, table_schema):
+		self.db_path = db_path
+		self.table_name = table_name
+		self.table_schema = table_schema
+		self.conn = sqlite3.connect(self.db_path)
+
+		self.conn.execute(f"CREATE TABLE IF NOT EXISTS {self.table_name} (id INTEGER AUTO INCREMENT, {self.table_schema});")
+
+	#TODO: PROTECT AGAINST SQL INJECTION!!!! URGENT!!!
+
+	def pop(self, table, query): #pull latest in table
+		return self.conn.execute(f"SELECT * FROM {table} WHERE {query} ORDER BY id DESC LIMIT 1;")
+	def pull(self, table, query): #pull oldest in table
+		return self.conn.execute(f"SELECT * FROM {table} WHERE {query} LIMIT 1;") #fix this these sqls are garbage
+	
+	def stack(self, table, query): #push to table
+		self.conn.execute(f"INSERT INTO {table} VALUES ({query});")#!!
+	def check(self, table, query):
+		return self.conn.execute(f"SELECT * FROM {table} WHERE {query} LIMIT 1;")
+	
+	#def execute(self, query):
+		#self.conn.execute(query)
 
 class CustomDB:
-    def __init__(self, db_path, table_name, table_schema):
-        self.db_path = db_path
-        self.table_name = table_name
-        self.table_schema = table_schema
-        self.conn = sqlite3.connect(self.db_path)
-
-        self.conn.execute(f"CREATE TABLE IF NOT EXISTS {self.table_name} ({self.table_schema});")
-        self.inc = self.conn.execute(f"SELECT * FROM information_schema.columns WHERE table_name = '{self.table_name}' LIMIT 1;")
-
-    #TODO: PROTECT AGAINST SQL INJECTION!!!! URGENT!!!
-
-    def pop(self, table, query): #pull latest in table
-        return self.conn.execute(f"SELECT * FROM {table} WHERE {query} ORDER BY {self.inc} DESC LIMIT 1;")
-    def pull(self, table, query): #pull oldest in table
-        return self.conn.execute(f"SELECT * FROM {table} WHERE {query} LIMIT 1;") #fix this these sqls are garbage
-    def stack(self, query): #push to table
-        self.conn.execute(f"INSERT INTO {self.table_name} VALUES ({query});")#!!
-    def execute(self, query):
-        self.conn.execute(query)
-        self.conn.commit()
-    def close(self):
-        self.conn.close()
+	pass
